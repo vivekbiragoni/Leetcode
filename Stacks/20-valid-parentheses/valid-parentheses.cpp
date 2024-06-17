701856275f22be87d9514c9ea6bc5526257fef70
vivekbiragoni <vivekbiragoni@gmail.com>
2024-06-17 17:10:29 +0530

    folder ajustments according to topics

--- Stacks/20-valid-parentheses/valid-parentheses.cpp
+++ Stacks/20-valid-parentheses/valid-parentheses.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int> st;
-        for(auto x : s){
-            if(x=='(' || x == '{' || x == '['){
-                st.push(x);
-            }
-            else if(x ==')' || x =='}' || x == ']'){
-                if(st.empty()){
-                    return false;
+        stack<char> st;
+        
+        for(auto x: s){
+            if(x=='('||x=='{'||x=='['){
+                st.push(x);  
+            } else {
+                
+                if(st.empty()) {
+                    return false;  
                 }
-                char y = st.top();
-                st.pop();
-                if ((x == ')' && y != '(' ) || (x=='}' && y != '{') || (x==']' && y!= '[')){
-                    return false;
+                char top = st.top();
+                if((x == ')' && top == '(') || (x == '}' && top == '{') || (x == ']' && top == '[')) {
+                    st.pop();  
+                } else {
+                    return false;  
                 }
-
             }
         }
-        return st.empty();
-
+        return st.empty();  
     }
 };
