8f514633d933be3b32a764435bba04c46152d185
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-06-20 10:26:46 +0530

    Time: 9 ms (57.78%) | Memory: 18.4 MB (70.86%) - LeetSync

--- Arrays/229-majority-element-ii/majority-element-ii.cpp
+++ Arrays/229-majority-element-ii/majority-element-ii.cpp
@@ -1,19 +1,38 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        unordered_map<int, int> umap;
-        vector<int> result;
+        int candidate1 = INT_MAX, candidate2 = INT_MAX;
+        int count1 = 0, count2 = 0;
+
         for(int num: nums){
-            if(umap.find(num) != umap.end()){
-                umap[num]++;
+            if(num == candidate1){
+                count1++;
+            }else if(num == candidate2){
+                count2++;
+            }else if(count1 == 0){
+                candidate1 = num;
+                count1 =1;
+            }else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
             }else{
-                umap[num]=1;
+                count1--;
+                count2--;
             }
         }
-        for(auto & pair : umap){
-            if(pair.second > nums.size()/3){
-                result.push_back(pair.first);
-            }
+
+        count1 = 0; count2 = 0;
+        for(int num : nums){
+            if(num == candidate1) count1++;
+            else if(num == candidate2) count2++;
+        }
+
+        vector<int> result;
+        if(count1>nums.size()/3){
+            result.push_back(candidate1);
+        }
+        if(count2> nums.size()/3 && candidate1 != candidate2){
+            result.push_back(candidate2);
         }
         return result;
     }
