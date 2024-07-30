7b27eb97acfb295233b71d437e39582b4f73eba2
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-07-30 10:03:28 +0530

    Time: 16 ms (83.03%) | Memory: 77.4 MB (26.68%) - LeetSync

---
+++ Slide-TwoPtr/2-add-two-numbers/add-two-numbers.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* addTwoRec(ListNode* l1, ListNode* l2, int carry){
+        if(!l1 && !l2 && carry == 0){
+            return nullptr;
+        }
+        int sum = carry;
+        if(l1){
+            sum += l1->val;
+            l1 = l1->next;
+        }
+        if(l2){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+
+        ListNode* resultNode = new ListNode(sum%10);
+        resultNode->next = addTwoRec(l1, l2, sum/10);
+        return resultNode;
+    }
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        return addTwoRec(l1, l2, 0);   
+    }
+};
