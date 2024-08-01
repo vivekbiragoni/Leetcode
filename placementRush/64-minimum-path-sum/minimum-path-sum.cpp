13f059a3ce70b48afbd24ffab161c14a9f551289
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-01 20:54:11 +0530

    Time: 8 ms (35.83%) | Memory: 13 MB (8.22%) - LeetSync

---
+++ placementRush/64-minimum-path-sum/minimum-path-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int helper(int i, int j, vector<vector<int>>& dp, vector<vector<int>> &grid) {
+        if (i == 0 && j == 0)
+            return grid[0][0];
+        if (i < 0 || j < 0)
+            return INT_MAX;
+        if (dp[i][j] != -1)
+            return dp[i][j];
+        int up = INT_MAX, left = INT_MAX;
+        if (i > 0) {
+            up = grid[i][j] + helper(i - 1, j, dp, grid);
+        }
+        if (j > 0) {
+            left = grid[i][j] + helper(i, j - 1, dp, grid);
+        }
+
+        return dp[i][j] = min(up, left);
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m - 1, n - 1, dp, grid);
+    }
+};
