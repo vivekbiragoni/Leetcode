8ca484294782b168280f85a8ea7aa1c7b2f448c7
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-01 19:28:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (8.28%) - LeetSync

--- Slide-TwoPtr/62-unique-paths/unique-paths.cpp
+++ Slide-TwoPtr/62-unique-paths/unique-paths.cpp
@@ -1,13 +1,15 @@
 class Solution {
-
 public:
+    int helper(int i, int j,vector<vector<int>> &dp){
+        if( i == 0 && j == 0) return 1;
+        if( i < 0 || j < 0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int up = helper(i-1, j, dp);
+        int left = helper(i, j-1, dp);
+        return dp[i][j] = up + left;
+    }
     int uniquePaths(int m, int n) {
-        int N = m + n - 2;
-        int r = min(m-1, n-1);
-        double result = 1;
-        for(int i = 1; i <= r; i++){
-            result = result * (N-r+i)/i;
-        }
-        return (int)result;
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return helper(m-1, n-1, dp);
     }
 };
