049e078adb86c6837a2e37d7fe46c72d6a083b0c
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-03 11:08:36 +0530

    Time: 17 ms (26.57%) | Memory: 19.9 MB (52.85%) - LeetSync

--- placementRush/518-coin-change-ii/coin-change-ii.cpp
+++ placementRush/518-coin-change-ii/coin-change-ii.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int f(int ind, int target,  vector<int>& coins, vector<vector<int>> &dp){
-        if(ind==0){
-            return (target%coins[0]==0);
-        }
-
-        if(dp[ind][target]!= -1) return dp[ind][target];
-        int notTake = f(ind-1, target, coins, dp);
-        int take = 0; 
-        if(coins[ind]<=target){
-            take = f(ind, target-coins[ind], coins, dp);
-        }
-        return dp[ind][target] = take+notTake;
-    }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
+        vector<vector<int>> dp(n, vector<int>(amount+1, 0));
 
-        return f(n-1, amount, coins, dp);
+        for(int target = 0; target <= amount; target++){
+            if(target%coins[0]==0){
+                dp[0][target]=1;
+            }
+        }
+        for(int ind = 1; ind < n; ind++){
+            for(int target = 0; target <= amount; target++){
+                int notTake = dp[ind-1][target];
+                int take = 0;
+                if(coins[ind]<= target) {
+                    take = dp[ind][target-coins[ind]];
+                }
+                dp[ind][target] = take + notTake;
+            }
+        }
+
+        return  dp[n-1][amount];
     }
 };
