17310c28f13ae950627c71d417cbac6324432f76
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-03 12:53:21 +0530

    Time: 16 ms (88.83%) | Memory: 8 MB (90.46%) - LeetSync

--- placementRush/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ placementRush/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -3,18 +3,19 @@ public:
     int longestCommonSubsequence(string text1, string text2) {
         int l1 = text1.size();
         int l2 = text2.size();
-        vector<vector<int>> dp(l1+1, vector<int>(l2+1, 0));
+        vector<int> prev(l2+1, 0), cur(l2+1, 0);
 
         for (int ind1 = 1; ind1 <= l1; ind1++) {
             for (int ind2 = 1; ind2 <= l2; ind2++) {
                 if (text1[ind1-1] == text2[ind2-1]) {
-                    dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1];
+                    cur[ind2] = 1 + prev[ind2 - 1];
                 } else {
-                    dp[ind1][ind2] = 0 + max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]);
+                    cur[ind2] = 0 + max(prev[ind2], cur[ind2 - 1]);
                 }
             }
+            prev = cur;
         }
-        return dp[l1][l2];
+        return cur[l2];
         
     }
 };
