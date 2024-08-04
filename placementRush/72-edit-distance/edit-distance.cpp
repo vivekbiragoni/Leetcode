62a9ab34908281fd153d3fd173bab0346a4c290f
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-04 19:34:05 +0530

    Time: 50 ms (15.82%) | Memory: 81.7 MB (11.24%) - LeetSync

---
+++ placementRush/72-edit-distance/edit-distance.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int help(string s1, string s2, int i , int j,  vector<vector<int>> &dp){
+
+        //base
+        if(i<0) return j+1;  //insert in s1
+        if(j<0) return i+1; // delete in s1
+
+        //mem check
+        if(dp[i][j]!=-1) return dp[i][j];
+
+        //all possibilities
+        if(s1[i]==s2[j]) return dp[i][j] =  0 + help(s1, s2, i-1, j-1, dp);
+        int ins = 1 + help(s1, s2, i, j-1, dp);
+        int del = 1 + help(s1, s2, i-1, j, dp);
+        int rep = 1 + help(s1, s2, i-1, j-1, dp);
+
+        return dp[i][j] = min(ins, min(del, rep));
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        return help(word1, word2, n-1, m-1, dp);
+    }
+};
