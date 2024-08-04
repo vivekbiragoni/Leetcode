a90fb8bec80af98bd2f7c3f55654fe5cb06afb97
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-04 19:41:53 +0530

    Time: 41 ms (19.84%) | Memory: 81.8 MB (10.22%) - LeetSync

--- placementRush/72-edit-distance/edit-distance.cpp
+++ placementRush/72-edit-distance/edit-distance.cpp
@@ -3,14 +3,14 @@ public:
     int help(string s1, string s2, int i , int j,  vector<vector<int>> &dp){
 
         //base
-        if(i<0) return j+1;  //insert in s1
-        if(j<0) return i+1; // delete in s1
+        if(i==0) return j;  //insert in s1
+        if(j==0) return i; // delete in s1
 
         //mem check
         if(dp[i][j]!=-1) return dp[i][j];
 
         //all possibilities
-        if(s1[i]==s2[j]) return dp[i][j] =  0 + help(s1, s2, i-1, j-1, dp);
+        if(s1[i-1]==s2[j-1]) return dp[i][j] =  0 + help(s1, s2, i-1, j-1, dp);
         int ins = 1 + help(s1, s2, i, j-1, dp);
         int del = 1 + help(s1, s2, i-1, j, dp);
         int rep = 1 + help(s1, s2, i-1, j-1, dp);
@@ -20,7 +20,7 @@ public:
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return help(word1, word2, n-1, m-1, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return help(word1, word2, n, m, dp);
     }
 };
