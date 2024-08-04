3ff9c55583cda8dfc3e43421169f95175190bad1
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-04 19:55:44 +0530

    Time: 4 ms (83.61%) | Memory: 13 MB (27.66%) - LeetSync

--- placementRush/72-edit-distance/edit-distance.cpp
+++ placementRush/72-edit-distance/edit-distance.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    int help(string s1, string s2, int i , int j,  vector<vector<int>> &dp){
-
-        //base
-        if(i==0) return j;  //insert in s1
-        if(j==0) return i; // delete in s1
-
-        //mem check
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        //all possibilities
-        if(s1[i-1]==s2[j-1]) return dp[i][j] =  0 + help(s1, s2, i-1, j-1, dp);
-        int ins = 1 + help(s1, s2, i, j-1, dp);
-        int del = 1 + help(s1, s2, i-1, j, dp);
-        int rep = 1 + help(s1, s2, i-1, j-1, dp);
-
-        return dp[i][j] = min(ins, min(del, rep));
-    }
     int minDistance(string word1, string word2) {
         int n = word1.size();
         int m = word2.size();
-        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
-        return help(word1, word2, n, m, dp);
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+        // base
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= m; j++)
+            dp[0][j] = j;
+
+        // tabulate
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                else {
+                    int ins = 1 + dp[i][j - 1];
+                    int del = 1 + dp[i - 1][j];
+                    int rep = 1 + dp[i - 1][j - 1];
+                    dp[i][j] = min(ins, min(del, rep));
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
