f18707663b030231800845e012ba1a85579610d6
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-04 18:44:07 +0530

    Time: 17 ms (65.56%) | Memory: 31.5 MB (30.53%) - LeetSync

--- placementRush/115-distinct-subsequences/distinct-subsequences.cpp
+++ placementRush/115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
-    int helper(string s, string t, int i, int j,  vector<vector<int>> &dp){
-        //base
-        if(j==0) return 1;
-        if(i==0) return 0;
-
-        if(dp[i][j]!=-1) return dp[i][j]; 
-
-        if(s[i-1]==t[j-1]){
-            return dp[i][j] = helper(s, t, i-1, j-1, dp) + helper(s, t, i-1, j, dp);
-        }else{
-            return dp[i][j] =  helper(s, t, i-1, j, dp);
-        }
-
-
-    }
     int numDistinct(string s, string t) {
-        int m  = s.size();
-        int n  = t.size();
+        int m = s.size();
+        int n = t.size();
 
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return helper(s, t, m, n, dp);
+        vector<vector<double>> dp(m + 1, vector<double>(n + 1, 0));
+
+        // base case
+        for (int i = 0; i < m; i++)
+            dp[i][0] = 1;
+
+
+        // changing params
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+
+                // recursion
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+
+        }
+        return (int)dp[m][n];
     }
 };
