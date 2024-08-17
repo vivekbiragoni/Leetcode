2785e41c35bdeccba03415db5ad902a7b42bcff2
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-17 17:02:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Multiplications to reach End/minimum-multiplications-to-reach-end.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    int minimumMultiplications(vector<int>& arr, int start, int end) {
+        // code here
+        if (start == end) return 0;
+        queue<pair<int,int>> q;
+        vector<int> dist(100000, 1e9);
+        dist[start] = 0;
+        q.push({ start,0});
+        
+        while(!q.empty()){
+            auto it = q.front();
+            q.pop();
+            
+            int node = it.first;
+            int steps = it.second;
+            int MOD = 100000;
+            for(auto runner: arr){
+                int num = (node * runner)%MOD;
+                if(steps+1 < dist[num]){
+                    dist[num]=steps+1;
+                    if(num==end) return steps+1;
+                    q.push({num, steps+1});
+                }
+            }
+            
+        }
+        return -1;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        int start, end;
+        cin >> start >> end;
+        Solution obj;
+        cout << obj.minimumMultiplications(arr, start, end) << endl;
+    }
+}
+
+// } Driver Code Ends
