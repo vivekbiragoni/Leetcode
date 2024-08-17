092c31011084c1100c8fd0b46d899b8a0d9f9869
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-17 15:29:28 +0530

    Time: 92 ms (28.01%) | Memory: 114.5 MB (44.76%) - LeetSync

---
+++ placementRush/334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& nums) {
+        int n = nums.size();
+        int a = INT_MAX, b = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= a)
+                a = nums[i];
+            else if (nums[i] <= b)
+                b = nums[i];
+            else {
+                return true;
+            }
+        }
+        return false;
+    }
+};
