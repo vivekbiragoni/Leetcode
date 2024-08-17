fc41755897c4282be8e0f475083642e0de1e2e8e
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-17 15:59:01 +0530

    Time: 7 ms (98.81%) | Memory: 34.6 MB (49.16%) - LeetSync

--- placementRush/724-find-pivot-index/find-pivot-index.cpp
+++ placementRush/724-find-pivot-index/find-pivot-index.cpp
@@ -2,22 +2,17 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         int n = nums.size();
-        vector<int> prefix(n,-1);
-        int cumsum = 0;
+
+        int totsum = 0, leftsum = 0;
+
+        for(int num : nums){
+            totsum += num;
+        }
         for(int i = 0; i< n; i++){
-            cumsum=nums[i]+cumsum;
-            prefix[i] = cumsum;
-        }
-        vector<int>suffix(n, -1);
-        int cumsum2 = 0;
-        for(int i = n-1; i>=0; i-- ){
-            cumsum2=nums[i]+cumsum2;
-            suffix[i]=cumsum2;
-        }
-        for(int i= 0; i< n; i++){
-            if(prefix[i]==suffix[i]){
+            if(leftsum == totsum-nums[i]-leftsum){
                 return i;
             }
+            leftsum += nums[i];
         }
         return -1;
     }
