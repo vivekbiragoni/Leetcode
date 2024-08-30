ad9e513e70e6b6adc2c6e1ed5c35207cce34ef40
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-08-30 15:44:56 +0530

    Time: 15 ms (94.57%) | Memory: 94.8 MB (65.98%) - LeetSync

--- placementRush/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ placementRush/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -6,27 +6,31 @@ public:
         int totalSize = n1 + n2;
         int medianIndex = totalSize / 2;
         vector<int> merged;
-        int i = 0, j = 0;
-        while (i < n1 && j < n2 && merged.size() <= medianIndex) {
-            if (nums1[i] < nums2[j]) {
-                merged.push_back(nums1[i++]);
+        int i = 0, j = 0, count = 0;
+        int prev = 0, current = 0;
+        
+        while (count <= medianIndex) {
+            prev = current;
+            if (i == n1) {
+                current = nums2[j];
+                j++;
+            } else if (j == n2) {
+                current = nums1[i];
+                i++;
+            } else if (nums1[i] < nums2[j]) {
+                current = nums1[i];
+                i++;
             } else {
-                merged.push_back(nums2[j++]);
+                current = nums2[j];
+                j++;
             }
+            count++;
         }
-
-        while (i < n1 && merged.size() <= medianIndex) {
-            merged.push_back(nums1[i++]);
-        }
-
-        while (j < n2 && merged.size() <= medianIndex) {
-            merged.push_back(nums2[j++]);
-        }
-
+        
         if (totalSize % 2 == 0) {
-            return (merged[medianIndex - 1] + merged[medianIndex]) / 2.0;
+            return (prev + current) / 2.0;
         } else {
-            return merged[medianIndex];
+            return current;
         }
     }
 };
