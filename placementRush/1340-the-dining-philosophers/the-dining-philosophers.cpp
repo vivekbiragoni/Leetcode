dad3f75dd41c4f3447eefc39e6e03928ae25bfb1
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-09-10 16:47:18 +0530

    Time: 55 ms (5.14%) | Memory: 17.2 MB (17.26%) - LeetSync

--- placementRush/1340-the-dining-philosophers/the-dining-philosophers.cpp
+++ placementRush/1340-the-dining-philosophers/the-dining-philosophers.cpp
@@ -1,21 +1,9 @@
 class DiningPhilosophers {
-private: 
-    mutex mtx;
+private:
+    mutex mtx[5];
 public:
-    DiningPhilosophers() {
-
+    void wantsToEat(int philosopher, function<void()> pickLeftFork, function<void()> pickRightFork, function<void()> eat, function<void()> putLeftFork, function<void()> putRightFork) {
+        scoped_lock lck(mtx[philosopher], mtx[(philosopher + 1) % 5]);
+        pickLeftFork(); pickRightFork(); eat(); putLeftFork(); putRightFork();
     }
-
-    void wantsToEat(int philosopher, function<void()> pickLeftFork,
-                    function<void()> pickRightFork, function<void()> eat,
-                    function<void()> putLeftFork,
-                    function<void()> putRightFork) {
-                        mtx.lock();
-                        pickLeftFork();
-                        pickRightFork();
-                        eat();
-                        putLeftFork();
-                        putRightFork();
-                        mtx.unlock();
-                    }
 };
