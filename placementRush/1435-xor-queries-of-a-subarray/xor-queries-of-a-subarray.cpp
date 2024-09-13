bf24bbc84c7778c35ed8b5dc818394497d294853
vivekbiragoni <104636857+vivekbiragoni@users.noreply.github.com>
2024-09-13 10:03:22 +0530

    Time: 58 ms (68.57%) | Memory: 42.1 MB (42.84%) - LeetSync

--- placementRush/1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
+++ placementRush/1435-xor-queries-of-a-subarray/xor-queries-of-a-subarray.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
+        int n = arr.size(), qsize= queries.size();
+        vector<int> prefix(n+1,0);
+        // prefix[0]=arr[0];
+        for(int i = 0; i< n; i++){
+            prefix[i+1]=arr[i]^prefix[i];
+        }
         vector<int> ans;
-        for(auto querie:queries){
-            auto left = querie[0];
-            auto right = querie[1];
-            auto xorr = arr[left];
-            for(int i = left+1; i<=right;i++){
-                xorr^=arr[i];
-            }
-            ans.push_back(xorr);
+        for(const auto& q: queries){
+            ans.push_back(prefix[q[1]+1]^prefix[q[0]]);
         }
         return ans;
     }
